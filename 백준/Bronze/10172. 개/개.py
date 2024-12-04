eb95a36512afc44d78cb9d8c41a3f617ec134c21
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2024-12-04 19:21:48 +0900

    [Bronze V] Title: 개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+# a, b, c = list(map(int, input().split()))
+
+print('|\_/|')
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
+
