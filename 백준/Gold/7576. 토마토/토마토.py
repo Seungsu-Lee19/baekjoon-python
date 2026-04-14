6456d9f8b4406fa14555398dbd18c3ceeb1dd45d
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-04-14 14:47:10 +0900

    [Gold V] Title: 토마토, Time: 428 ms, Memory: 164820 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,55 +1,51 @@
 from collections import deque
 
-m, n = map(int, input().split()) # m은 열, n은 행
+N, M = map(int, input().split())
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+tomato = []
+good = deque()
+total = 0
+day = [[-1 for _ in range(N)] for _ in range(M)]
+for i in range(M):
+    tom = list(map(int, input().split()))
+    for j in range(N):
+        if tom[j] == 0:
+            total += 1
+        elif tom[j] == 1:
+            total += 1
+            good.append((i, j))   
+            day[i][j] = 0
+        else:
+            day[i][j] = 0
+    tomato.append(tom)
 
-def bfs(graph, queue):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    cnt = -1
-    
-    while queue:
-        x = []
-        y = []
-        while queue:
-            _x, _y = queue.popleft()
-            x.append(_x)
-            y.append(_y)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs():
+    while good:
+        x, y = good.popleft()
         
-        for _x, _y in zip(x, y):
-            for i in range(4):
-                nx = _x + dx[i]
-                ny = _y + dy[i]
-                
-                if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                    continue
-                if graph[nx][ny] == -1:
-                    continue
-                
-                if graph[nx][ny] == 0:
-                    graph[nx][ny] = 1
-                    queue.append((nx, ny))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
             
-        cnt += 1
-        
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                print(-1)
-                return
+            if nx < 0 or ny < 0 or nx >= M or ny >= N:
+                continue
             
-    print(cnt)
+            if tomato[nx][ny] == 0 and day[nx][ny] == -1:
+                day[nx][ny] = day[x][y] + 1
+                good.append((nx, ny))
+                # tomato[nx][ny] = 1
 
-queue = deque()
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            queue.append((i, j))
+def check():
+    _max = -1
+    for i in range(M):
+        for j in range(N):
+            if day[i][j] == -1:
+                return -1
+            _max = max(_max, day[i][j])
+    return _max
 
-bfs(graph, queue)
-        
-    
+bfs()
+print(check())
