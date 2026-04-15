eb43e5124c0b49f399b32501fd34126e8c74c49a
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-04-15 13:44:51 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+def dynamic(n):
+    dp = [0 for _ in range(11)]
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    
+    for i in range(4, 11):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    
+    return dp[n]
+for _ in range(T):
+    n = int(input())
+    print(dynamic(n))
+    
