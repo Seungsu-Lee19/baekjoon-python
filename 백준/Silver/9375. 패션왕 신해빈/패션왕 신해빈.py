e4f61cb8dcea9c5b527b48df6b05e6f2364bbf18
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-04-15 14:23:12 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,32 @@
+T = int(input())
+
+def dynamic(n):
+    dp = [0 for _ in range(11)]
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    
+    for i in range(4, 11):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    
+    return dp[n]
+for _ in range(T):
+    n = int(input())
+    
+    day = 1
+    kind_n = 0
+    clothes = {}
+    for _ in range(n):
+        name, kind = input().split()
+        if kind in clothes:
+            clothes[kind] += 1
+        else:
+            clothes[kind] = 1
+            kind_n += 1
+            
+    for k in clothes:
+        day = day * (clothes[k] + 1)
+        
+    print(day - 1)
+    
+    
