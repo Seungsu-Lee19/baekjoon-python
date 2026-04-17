315d65018541ddacd0ed4c919133feed1e1de2ed
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-04-17 16:34:12 +0900

    [Silver I] Title: IOIOI, Time: 116 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,34 +1,21 @@
 N = int(input())
 M = int(input())
 S = input()
-# find_str = ''
-# for i in range(2*N + 1):
-#     if i % 2 == 0:
-#         find_str += 'I'
-#     else:
-#         find_str += 'O'
 
 cnt = 0
-i = 0   
-while i < M - (2 * N):
-    if S[i] == 'I':
-        good = True
-        for j in range(i, i + 2*N + 1):
-            find_s = None
-            if (j - i) % 2 == 0:
-                find_s = 'I'
-            else:
-                find_s = 'O'
-            
-            if find_s != S[j]:
-                good = False
-                break
+cur = 0
+i = 1
+
+while i < M - 1:
+    if S[i - 1] == 'I' and S[i] == 'O' and S[i + 1] == 'I':
+        cur += 1
         
-        if good:
+        if cur >= N:
             cnt += 1
-            i = i + 2
-            continue
         
-    i += 1
-                
+        i += 2
+    else:
+        cur = 0
+        i += 1
+
 print(cnt)
