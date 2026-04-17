865c4d10f4ebc1db50564e79b0f310370d1b684e
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-04-17 11:40:04 +0900

    [Silver II] Title: 좌표 압축, Time: 1124 ms, Memory: 260092 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,29 +1,15 @@
-import heapq
+from bisect import bisect_left
+import sys
 
-N = int(input())
-x = list(map(int, input().split()))
-min_heap = []
-ans = [0 for _ in range(N)]
+input = sys.stdin.readline
+
+N = int(input().strip())
+
+X = list(map(int, input().split()))
+
+coordinate = sorted(list(set(X)))
+
+ans = {}
 
 for i in range(N):
-    heapq.heappush(min_heap, (x[i], i))
-
-prev_x = None
-prev_i = None
-
-while min_heap:
-    _x, i = heapq.heappop(min_heap)
-    
-    if prev_x is None:
-        prev_x = _x
-        prev_i = i
-        ans[i] = 0
-    else:
-        if _x == prev_x:
-            ans[i] = ans[prev_i]
-        else:
-            ans[i] = ans[prev_i] + 1
-            prev_x = _x
-            prev_i = i
-
-print(*ans) 
+    print(bisect_left(coordinate, X[i]), end=' ')
